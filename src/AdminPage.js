2cbebc0320bee3c66f31f999ee38da0129fab8b5
Aravind <aravind@pop-os.localdomain>
2021-03-09 15:31:37 +0530

    issue 3 fixed

--- src/AdminPage.js
+++ src/AdminPage.js
@@ -1,18 +1,39 @@
 import React from "react";
+import axios from "axios";
+import API from "./config/api";
 
 import { userService } from "./services/user.service";
-
+import { Button, Col, Form, FormControl, Row } from "react-bootstrap";
 export default class AdminPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       users: null,
+      userId: "",
+      adminRole: "",
     };
   }
 
   componentDidMount() {
     userService.getAll().then((users) => this.setState({ users }));
   }
+  onroleHandler = (value, id) => {
+    this.setState({ userId: id });
+    this.setState({ adminRole: value });
+  };
+
+  onUpdateHandler = () => {
+    console.log(this.state.userId, this.state.adminRole);
+    this.setState({ changeForAdminId: "" });
+    this.setState({ adminRole: "" });
+    API.post(`/api/users/userId`, {
+      role: this.state.adminRole,
+      userId: this.state.userId,
+    }).then((res) => {
+      // console.log(res.data);
+      userService.getAll().then((users) => this.setState({ users }));
+    });
+  };
 
   render() {
     const { users } = this.state;
@@ -27,7 +48,33 @@ export default class AdminPage extends React.Component {
             <ul>
               {users.map((user) => (
                 <li key={user.id}>
-                  {user.firstName} {user.lastName}{" "}
+                  {user.firstName} {user.lastName} role:{" "}
+                  {user.roles[0].Role.name}
+                  <Row>
+                    <Col>
+                      <Form.Group
+                        controlId="exampleForm.ControlSelect1"
+                        style={{ width: "30vw" }}
+                      >
+                        <Form.Label>Change role:</Form.Label>
+                        <FormControl
+                          as="select"
+                          defaultValue={user.roles[0].Role.name}
+                          onChange={(e) =>
+                            this.onroleHandler(e.target.value, user.id)
+                          }
+                        >
+                          <option>User</option>
+                          <option>Admin</option>
+                        </FormControl>
+                      </Form.Group>
+                    </Col>
+                    <Col>
+                      <Button onClick={() => this.onUpdateHandler()}>
+                        Update
+                      </Button>
+                    </Col>{" "}
+                  </Row>
                 </li>
               ))}
             </ul>
