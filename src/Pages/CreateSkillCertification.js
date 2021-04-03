df779871849d1df552241460b946a9e20ce7a8be
Bharath S <deepssekar1729@gmail.com>
2021-04-03 21:30:32 +0530

    Completed Create Skill Certification Screen

--- src/Pages/CreateSkillCertification.js
+++ src/Pages/CreateSkillCertification.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Grid,
@@ -7,40 +7,110 @@ import {
   Paper,
   Card,
   Link,
+  makeStyles,
+  Modal,
 } from "@material-ui/core";
+import ListLookupSkill from "../Pages/Solutions/ListLookupSkill";
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 500,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    top: `30vh`,
+    left: `38%`,
+    transform: `translate(-15%, -38%)`,
+  },
+}));
 function CreateSkillCertification() {
-  return (
-    <div style={{ padding: "20px" }}>
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const body = (
+    <Paper elevation={3} className={classes.paper}>
       <Typography variant="h6" gutterBottom>
-        Create Skill Certification
+        Skill Lookup
       </Typography>
       <Grid container spacing={2}>
-        <Grid item xs={10}>
+        <Grid item xs={9}>
           <TextField
             size="small"
             id="outlined-select-currency"
-            select
-            label="Skill"
             fullWidth={true}
+            label="Stack"
             // value={currency}
             // onChange={handleChange}
-            // helperText="Please select your currency"
+            // helperText="Please select stack type"
             variant="outlined"
-          >
-            <option aria-label="None" value="" />
-            <option value={1}>Java</option>
-            <option value={2}>HTML</option>
-            <option value={3}>CSS</option>
-          </TextField>
+          ></TextField>
         </Grid>
-
-        <Grid item xs={2}>
+        <Grid item xs={3}>
           <Button variant="contained" color="primary">
-            Lookup Skill
+            Search
           </Button>
         </Grid>
       </Grid>
+      <ListLookupSkill></ListLookupSkill>
+    </Paper>
+  );
 
+  return (
+    <div style={{ padding: "20px" }}>
+      <Paper
+        elevation={0}
+        square
+        style={{
+          borderBottomStyle: "solid",
+          borderBottomWidth: "1px",
+          paddingBottom: "2vh",
+        }}
+      >
+        <Typography variant="h6" gutterBottom>
+          Create Skill Certification
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={10}>
+            <TextField
+              size="small"
+              id="outlined-select-currency"
+              select
+              label="Skill"
+              fullWidth={true}
+              // value={currency}
+              // onChange={handleChange}
+              // helperText="Please select your currency"
+              variant="outlined"
+            >
+              <option aria-label="None" value="" />
+              <option value={1}>Java</option>
+              <option value={2}>HTML</option>
+              <option value={3}>CSS</option>
+            </TextField>
+          </Grid>
+
+          <Grid item xs={2}>
+            <Button variant="contained" color="primary" onClick={handleOpen}>
+              Lookup Skill
+            </Button>
+          </Grid>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+          >
+            {body}
+          </Modal>
+        </Grid>
+      </Paper>
       <Typography variant="h6" style={{ marginTop: "3vh" }} gutterBottom>
         Knowledge
       </Typography>
@@ -192,6 +262,101 @@ function CreateSkillCertification() {
           </Button>
         </Grid>
       </Grid>
+      <Grid container style={{ marginTop: "3vh" }}>
+        <Grid item xs={3}>
+          Completion
+        </Grid>
+        <Grid item xs={1}>
+          50%
+        </Grid>
+        <Grid item xs={1}>
+          <Link style={{ textDecoration: "underline", color: "blue" }}> x</Link>
+        </Grid>
+      </Grid>
+      <Grid container style={{ marginTop: "1vh" }}>
+        <Grid item xs={3}>
+          Requirement Clarification
+        </Grid>
+        <Grid item xs={1}>
+          25%
+        </Grid>
+        <Grid item xs={1}>
+          <Link style={{ textDecoration: "underline", color: "blue" }}> x</Link>
+        </Grid>
+      </Grid>
+      <Grid container style={{ marginTop: "1vh" }}>
+        <Grid item xs={3}>
+          Coding Standards
+        </Grid>
+        <Grid item xs={1}>
+          25%
+        </Grid>
+        <Grid item xs={1}>
+          <Link style={{ textDecoration: "underline", color: "blue" }}> x</Link>
+        </Grid>
+      </Grid>
+      <Grid container style={{ marginTop: "3vh" }}>
+        <Grid item xs={3}>
+          Minimum tasks per objective
+        </Grid>
+
+        <Grid item xs={1}>
+          <TextField
+            size="small"
+            id="outlined-select-currency"
+            fullWidth={true}
+            // value={currency}
+            // onChange={handleChange}
+            // helperText="Please select stack type"
+            variant="outlined"
+          ></TextField>
+        </Grid>
+      </Grid>
+      <Grid container xs={10} style={{ marginTop: "3vh" }}>
+        <Grid item xs>
+          <TextField
+            size="small"
+            id="outlined-select-currency"
+            select
+            label="Maximum task complexity level"
+            fullWidth={true}
+            // value={currency}
+            // onChange={handleChange}
+            // helperText="Please select your currency"
+            variant="outlined"
+          >
+            <option aria-label="None" value="" />
+            <option value={1}>Easy</option>
+            <option value={2}>Medium</option>
+            <option value={3}>Hard</option>
+          </TextField>
+        </Grid>
+      </Grid>
+      <Typography variant="h6" style={{ marginTop: "3vh" }} gutterBottom>
+        Badge
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <TextField
+            size="small"
+            id="outlined-select-currency"
+            fullWidth={true}
+            label="graphics.jpeg"
+            // value={currency}
+            // onChange={handleChange}
+            // helperText="Please select stack type"
+            variant="outlined"
+          ></TextField>
+        </Grid>
+        <Grid item xs={2}>
+          <Button variant="contained" color="primary">
+            Browse..
+          </Button>
+        </Grid>
+      </Grid>
+      <Button variant="contained" style={{ marginTop: "3vh" }} color="primary">
+        Submit
+      </Button>
     </div>
   );
 }
